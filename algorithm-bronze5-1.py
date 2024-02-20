a9004c60f6cc6ee8e9fca618853bbe6d80c2cfe0
bellisajean <155508512+bellisajean@users.noreply.github.com>
2024-02-20 16:03:14 +0900

    Update algorithm-bronze5-1.py

--- algorithm-bronze5-1.py
+++ algorithm-bronze5-1.py
@@ -1,5 +1,5 @@
-import sys
 
-total_money, num_entities = 1000, 100
+total_money, num_entities = 1000, 100 
 money_per_entity, remaining_money = divmod(total_money, num_entities) # divmod : 내장함수
-print(money_per_entity, remaining_money)
+print(money_per_entity, remaining_money) # 1000원을 100원씩 10명에게 나눠주고 0이 남음
+# 출력 - 전체돈,남은돈
